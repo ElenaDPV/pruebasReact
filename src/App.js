d3c957ee24b8bd11c591f8b5a6b4abd4bd71d603
Elena <elena.ormaza@protonmail.com>
2021-09-20 15:02:33 +0200

    estilos

--- src/App.js
+++ src/App.js
@@ -5,50 +5,52 @@ function App() {
   return ( <div className="App">
 
 
-
-<nav class="navbar navbar-expand-lg navbar-black bg-black">
+<nav class="navbar navbar-expand-lg navbar-light navRose" >
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+   
+    <a class="navbar-brand d-flex" href="#">
+      <div class="logoBaGD"></div>
+    </a>
+
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+      <li class="nav-item">
+          <a class="nav-link nomWeb" aria-current="page" href="#">PLANTITAS</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link active" aria-current="page" href="#">TODAS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Interior</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Exterior
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">De Sol</a></li>
+            <li><a class="dropdown-item" href="#">De Sombra</a></li>
             
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
+       
       </ul>
-      <form class="d-flex">
+      <div>
+        <form class="d-flex">
+          <input class="form-control me-2 espacMarg" type="search" placeholder="Search" aria-label="Search"/>
+          <button class="btn btnRose" type="submit">Search</button>
+        </form>
+      </div>
       
-     
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        
-      
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
 
 <div class="p-5 mb-4 bg-light rounded-3">
-  <h1 class="h1Pruebas">PRUEBAS CON REACT</h1>
+  <h1 class="h1Pruebas">MIS PLANTAS</h1>
   <p class="pTexto1">
   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
   </p>
